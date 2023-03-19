bcece256133bf497653d26e580281f8280435a1b
Jonathan Bangala <jbangala90@gmail.com>
2023-03-19 17:53:59 +0200

    Under construction

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+
+function App() {
+
+  return (
+    <div className="app">
+      app
+    </div>
+  )
+};
+
+export default App;
